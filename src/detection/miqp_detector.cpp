0bce7a7e18937938aabd1f1f9774b636cc6f995f
Gregory Izatt <gizatt@mit.edu>
2017-01-03 14:32:14 -0500

    Split mccormick quaterion constraint into its own header

--- src/detection/miqp_detector.cpp
+++ src/detection/miqp_detector.cpp
@@ -35,6 +35,7 @@
 #include <pcl/common/transforms.h>
 
 #include "optimization_helpers.h"
+#include "rotation_helpers.h"
 
 using namespace std;
 using namespace Eigen;
@@ -121,7 +122,6 @@ int main(int argc, char** argv) {
   }
 
   // Center scene point cloud
-
   pcl::PointCloud<PointType>::Ptr scene_pts_tf (new pcl::PointCloud<PointType> ());
   Vector3d avg_scene_pt = Vector3d::Zero();
   for (int k=0; k<scene_pts->size(); k++){
@@ -158,44 +158,6 @@ int main(int argc, char** argv) {
   pcl::visualization::PointCloudColorHandlerCustom<PointType> scene_color_handler (scene_pts_tf, 255, 255, 128);
   viewer.addPointCloud<PointType>(scene_pts_tf, scene_color_handler, "scene pts"); 
   viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "scene pts");
-
-
-
-
-  /*
-  // Solves earth movers problem for scene to model cloud
-  // Crazy naive mixed integer question we're going to ask is
-  // assignment of points in model to points in scene
-  MathematicalProgram prog;
-  auto C = prog.AddBinaryVariables(scene_pts_tf->size(), model_pts_tf->size(), "c");
-
-  // Every scene (tactile) point should correspond to exactly 1 model point
-  Eigen::MatrixXd C1 = Eigen::MatrixXd::Ones(1, model_pts_tf->size());
-  for (size_t k=0; k<scene_pts_tf->size(); k++){
-    prog.AddLinearConstraint(C1, 1, 1, {C.block(k, 0, 1, model_pts_tf->size()).transpose()});
-  }
-
-  // Every model point should correspond to at most 1 scene point
-  Eigen::MatrixXd C2 = Eigen::MatrixXd::Ones(1, scene_pts_tf->size());
-  for (size_t k=0; k<model_pts_tf->size(); k++){
-    prog.AddLinearConstraint(C2, 0, 1, {C.block(0, k, scene_pts_tf->size(), 1)});
-  }
-
-  // Every correspondence is weighted by the DISTANCE of that correspondence -- shorter is better
-  for (int k_s=0; k_s<scene_pts_tf->size(); k_s++){
-    for (int k_m=0; k_m<model_pts_tf->size(); k_m++){
-      prog.AddLinearCost( drake::Vector1d(pointDistance(scene_pts_tf->at(k_s), model_pts_tf->at(k_m))), {C.block<1,1>(k_s, k_m)} );
-    }
-  }
-  double now = getUnixTime();
-  auto out = prog.Solve();
-  string problem_string = "earthmovers";
-  double elapsed = getUnixTime() - now;
-  */
-
-  //prog.PrintSolution();
-
-
 
 
   // Solve rigid body transform problem with correspondences
@@ -244,128 +206,7 @@ int main(int argc, char** argv) {
 
   bool free_rot = true;
   if (free_rot){
-    /*
-    // constrain rotations to SO(3) -- i.e.
-    // R.' R = I, and det(R) = +1
-    // ****************** TODO, MOVE THIS TO ROTATION CONSTRAINT CLASS
-    // Add core quaternion variables, ordered w x y z
-    auto Q = prog.AddContinuousVariables(4, 1, "q");
-    prog.AddBoundingBoxConstraint(-VectorXd::Ones(4), VectorXd::Ones(4), {Q});
-
-    // Add variables for bilinear quaternion element products
-    auto B = prog.AddContinuousVariables(10, 1, "b");
-    prog.AddBoundingBoxConstraint(-VectorXd::Ones(10), VectorXd::Ones(10), {B});   
-
-    // Constrain elements of rotation element by bilinear quaternion values
-    // This constrains the 9 elements of the rotation matrix against the 
-    // 10 bilinear terms in various combinations
-    MatrixXd Aeq(9, 9 + 10);
-    Aeq.setZero();
-    MatrixXd beq(9, 1);
-    beq.setZero();
-    // build some utility inds to make writing this cleaner...
-    int k=0;
-    char qnames[5] = "wxyz";
-    const int kNumRotVars = 9;
-    const int kOffww = kNumRotVars + 0;
-    const int kOffwx = kNumRotVars + 1;
-    const int kOffwy = kNumRotVars + 2;
-    const int kOffwz = kNumRotVars + 3;
-    const int kOffxx = kNumRotVars + 4;
-    const int kOffxy = kNumRotVars + 5;
-    const int kOffxz = kNumRotVars + 6;
-    const int kOffyy = kNumRotVars + 7;
-    const int kOffyz = kNumRotVars + 8;
-    const int kOffzz = kNumRotVars + 9;
-    // Todo: I know you can do this formulaicaijasafcally...
-    // R00 = w^2 + x^2 - y^2 - z^2
-    Aeq(k, 0) = 1.0;
-    Aeq(k, kOffww) = -1.0;
-    Aeq(k, kOffxx) = -1.0;
-    Aeq(k, kOffyy) = 1.0;
-    Aeq(k, kOffzz) = 1.0;
-    beq(k, 0) = 0.0;
-    k++;
-    // R01 = 2xy + 2wz -> R01 - 2xy - 2wz = 0
-    Aeq(k, 3) = 1.0;
-    Aeq(k, kOffxy) = -2.0;
-    Aeq(k, kOffwz) = -2.0;
-    beq(k, 0) = 0.0;
-    k++;
-    // R02 = 2xz - 2wy -> R02 - 2xz + 2wy = 0
-    Aeq(k, 6) = 1.0;
-    Aeq(k, kOffxz) = -2.0;
-    Aeq(k, kOffwy) = 2.0;
-    beq(k, 0) = 0.0;
-    k++;
-    // R10 = 2xy - 2wz -> R10 - 2xy + 2wz = 0
-    Aeq(k, 1) = 1.0;
-    Aeq(k, kOffxy) = -2;
-    Aeq(k, kOffwz) = 2;
-    beq(k, 0) = 0.0;
-    k++;
-    // R11 = w^2 - x^2 + y^2 - z^2
-    Aeq(k, 4) = 1.0;
-    Aeq(k, kOffww) = -1.0;
-    Aeq(k, kOffxx) = 1.0;
-    Aeq(k, kOffyy) = -1.0;
-    Aeq(k, kOffzz) = 1.0;
-    beq(k, 0) = 0.0;
-    k++;
-    // R12 = 2yz + 2wx -> r12 - 2yz - 2wx = 0
-    Aeq(k, 7) = 1.0;
-    Aeq(k, kOffyz) = -2.0;
-    Aeq(k, kOffwx) = -2.0;
-    beq(k, 0) = 0.0;
-    k++;
-    // R20 = 2xz + 2wy -> r20 - 2xz - 2wy = 0
-    Aeq(k, 2) = 1.0;
-    Aeq(k, kOffxz) = -2.0;
-    Aeq(k, kOffwy) = -2.0;
-    beq(k, 0) = 0.0;
-    k++;
-    // R21 = 2yz - 2wx -> r21 - 2yz + 2wx = 0
-    Aeq(k, 5) = 1.0;
-    Aeq(k, kOffyz) = -2.0;
-    Aeq(k, kOffwx) = 2.0;
-    beq(k, 0) = 0.0;
-    k++;
-    // R22 = w^2 - x^2 - y^2 + z^2
-    Aeq(k, 8) = 1.0;
-    Aeq(k, kOffww) = -1.0;
-    Aeq(k, kOffxx) = 1.0;
-    Aeq(k, kOffyy) = 1.0;
-    Aeq(k, kOffzz) = -1.0;
-    beq(k, 0) = 0.0;
-    k++;
-    prog.AddLinearEqualityConstraint(Aeq, beq, {flatten_MxN(R), B});
-    // Now constrain xx + yy + zz + ww = 1
-    prog.AddLinearEqualityConstraint(MatrixXd::Ones(1, 4), MatrixXd::Ones(1, 1), 
-      {B.block<1,1>(0,0),B.block<1,1>(4,0),B.block<1,1>(7,0),B.block<1,1>(9,0)});
-    // Finally, constrain each of the bilinear product pairs with their core quaternion variables
-    k=0;
-    for (int i=0; i<4; i++){
-      for (int j=i; j<4; j++){
-        // spawn new region selection variables
-        string corename; corename += qnames[i]; corename += qnames[j];
-
-        // select variable "x" and "y" out of quaternion
-        auto x = Q(i, 0);
-        auto y = Q(j, 0);
-        // and select bilinear product "xy" variable
-        auto xy = B(k,0);
-
-        add_McCormick_envelope(prog, xy, x, y, corename,
-                               -1.0, // xL
-                               1.0,  // xH
-                               -1.0, // yL
-                               1.0,  // yH
-                               4, 4); // M_x, M_y 
-        k++;
-      }
-    }
-    */
-    // ****************** TODO, MOVE THIS TO ROTATION CONSTRAINT CLASS
+    addMcCormickQuaternionConstraint(prog, R, 4, 4);
   } else {
     // constrain rotations to ground truth
     // I know I can do this in one constraint with 9 rows, but eigen was giving me trouble
